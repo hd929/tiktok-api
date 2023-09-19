07353b562c65da1601a1090e6f44af41b93aa91e
Akatsuki <handuy099@gmail.com>
2023-09-19 17:07:58 +0700

    [API/URL]: SSL in production only

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-        URL::forceScheme('https');
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        } else {
+            URL::forceScheme('http');
+        }
     }
 }
